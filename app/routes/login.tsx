05c4c0c656979959f6efc58d1bb0c03e97eab8d2
RashVV <vadim.ratushnyak@gmail.com>
2022-08-03 16:40:24 +0300

    set user login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -10,13 +10,12 @@ import {
 } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
-import { login } from "~/utils/session.server";
+import { login, createUserSession } from "~/utils/session.server";
 import stylesUrl from "../styles/login.css";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
-
 
 function validateUsername(username: unknown) {
   if (typeof username !== "string" || username.length < 3) {
@@ -86,13 +85,14 @@ export const action: ActionFunction = async ({
 
   switch (loginType) {
     case "login": {
-      // login to get the user
-      // if there's no user, return the fields and a formError
-      // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+      const user = await login({ username, password });// login to get the user
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Username/Password combination is incorrect`,
+        });
+      }
+      return createUserSession(user.id, redirectTo);
     }
     case "register": {
       const userExists = await db.user.findFirst({
@@ -104,9 +104,7 @@ export const action: ActionFunction = async ({
           formError: `User with username ${username} already exists`,
         });
       }
-      // create the user
-      // create their session and redirect to /jokes
-      return badRequest({
+        return badRequest({
         fields,
         formError: "Not implemented",
       });
@@ -120,29 +118,10 @@ export const action: ActionFunction = async ({
   }
 };
 
-export const action: ActionFunction = async ({
-  request,
-}) => {
+
   // ...
-  switch (loginType) {
-    case "login": {
-      const user = await login({ username, password });
-      console.log({ user });
-      if (!user) {
-        return badRequest({
-          fields,
-          formError: `Username/Password combination is incorrect`,
-        });
-      }
-      // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
-    }
-    // ...
-  }
-};
+
+     
 
 export default function Login() {
   const actionData = useActionData<ActionData>();
